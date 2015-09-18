b2b20e40e98c5fbe8516a71db6332b9c0264dfc6
Andres Breton <bretonics@users.noreply.github.com>
2015-09-17 22:47:53 -0400

    fixed symlink issue: not finding lib

--- scripter.pl
+++ scripter.pl
@@ -1,21 +1,19 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 
 use warnings; use strict; use diagnostics; use feature qw(say);
 use Getopt::Long; use Pod::Usage;
 
-use File::Basename qw(dirname);
-use Cwd qw(abs_path);
-use lib (dirname abs_path $0). "/lib";
+use FindBin; use lib "$FindBin::RealBin/lib";
 
-use header;
+use Header;
 
-# =============================================
-#   Automate coding files with default content
+# =============================================================================
+#   CAPITAN:  Andres Breton
+#   FILE: scripter.pl
+#   LICENSE:  GPL2
+#   USAGE: Automate file creation with default content/structure
 #
-# 	Created by: Andres Breton
-#	File:       scripter.pl ©
-#
-# =============================================
+# =============================================================================
 
 
 #-------------------------------------------------------------------------
@@ -52,8 +50,10 @@ checkARGV(@ARGV);
 #-------------------------------------------------------------------------
 # VARIABLES
 
+my $REALBIN = "$FindBin::RealBin";
 my $fileName = $ARGV[0]; chomp $fileName;
 my $perms = 0755; #what default file permissions do you want?
+my $USER = "Andres Breton"; #or $ENV{LOGNAME}
 
 my @fileExtensions = qw(pl pm py r rb c);    #File extensions available to write. Add extensions here and template for extension in sub "touchFile"
 my %templates = (pl=>"perl",pm=>"perl",py=>"python",
@@ -67,7 +67,7 @@ my $NC = "\e[0m";
 #-------------------------------------------------------------------------
 # CALLS
 
-newFile($fileName); 
+newFile($fileName);
 
 # -------------------------------------------------------------------------
 # SUBS
@@ -87,7 +87,7 @@ sub newFile {
     my ($ext) = $fileName =~ /.(\w+$)/;        #get file extension
     foreach my $extension (@fileExtensions) {    #search for file type in @fileExtensions
         if ($extension eq $ext) {
-            if(-e $fileName) {                  #check file exists
+            if(-e $fileName) {                  #check file exists. Do NOT overide
                 say "$fileName ${redTxt}not${NC} created, file already exists.\nTerminating...", $!;
                 exit;
             }
@@ -97,24 +97,23 @@ sub newFile {
     }
     say "You did not provide a proper file type. File extension provided was \"$ext\"\n";
     say "Valid extensions are:";
-    print join("\n",@fileExtensions), "\n\n";
+    print join(",",@fileExtensions), "\n\n";
 }
 
 sub touchFile   {               #file maker
     my ($fileName,$extension,$DESCRIPTION,$LICENSE) = @_;
     my ($name) = $fileName =~ /(\w+).$extension/;
-    my $user = "Andres Breton"; #or $ENV{LOGNAME}
-    
+
     unless (open(OUTFILE, ">", $fileName)) {     #check out file write access
         say "Unable to write file. Please check permissions";
         die "Can not open $fileName for writing.\n", $!;
     }
-    
-    # Search for template file matching extension provided 
+
+    # Search for template file matching extension provided
     foreach my $key (%templates) {
         if ($key eq $extension) {
             my $template = $templates{$extension}; #get template of file desired
-            open(SYSCALL,"cat templates/$template |") or die "Could not find template file $!"; #sys call
+            open(SYSCALL, "cat $REALBIN/templates/$template |") or die "Could not find template file $!"; #sys call
             while(<SYSCALL>){
                 print OUTFILE; #print template content to new file
             }
