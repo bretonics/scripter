a32a222e751146972c9f946f6714818fb9102634
Andres Breton <bretonics@users.noreply.github.com>
2016-05-26 21:12:16 -0400

    add header and close #3

--- lib/content.pm
+++ lib/content.pm
@@ -5,12 +5,21 @@ use warnings; use strict; use feature qw(say);
 use Exporter qw(import);
 our @ISA = qw(Exporter);
 our @EXPORT = qw(writeContent license); #functions exported by default
+our @EXPORT_OK = qw(); #functions exported on demand
+
+# ==============================================================================
+#
+#   CAPITAN: Andres Breton
+#   FILE: Content.pm
+#   USAGE: Get/write content from template files
+#
+# ==============================================================================
 
 sub writeContent {
     my ($fileName, $USER, $LICENSE, $REALBIN, $template, $OUTFILE) = @_;
-    open(SYSCALL, "cat $REALBIN/templates/$template |") or die "Could not find template file $!"; #sys call
-    while(<SYSCALL>){
-        # say $OUTFILE;
+    my $tempFile = "$REALBIN/templates/$template";
+    open(my $tempFileFH, $tempFile) or die "Could not find template file $tempFile", $!;
+    while(<$tempFileFH>){
         if ($_ =~ /^#\s+CAPITAN:/) {
             chomp $_;
             print $OUTFILE "$_ $USER\n"; next;
@@ -26,7 +35,7 @@ sub writeContent {
         }
         print $OUTFILE $_;
     }
-    close SYSCALL;
+    close $tempFileFH;
 }
 
 sub license {
